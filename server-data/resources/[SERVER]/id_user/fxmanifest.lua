30783fdf7ba38faf1bdc47fca7c8b82de58759e0
YourgenAP <kalandadzega@mail.ru>
2022-01-28 11:30:39 +0100

    Updated server-data

---
+++ server-data/resources/[SERVER]/id_user/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'rdr3'
+
+author 'WestRP Team - YourgenAP'
+description 'setting up id-user-name-surname corelation'
+version '1.0.0'
+
+resource_type 'gametype' 
+
+server_script 'server.lua'
