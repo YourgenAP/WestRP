7b7e4bd8ff3388e7a1cf137e977a74b2bf9d48a6
YourgenAP <kalandadzega@mail.ru>
2022-05-22 01:44:15 +0200

    Added VORP Core and dependencies

--- server-data/resources/[WESTRP]/westrp-core/server/classes/sv_users.lua
+++ server-data/resources/[WESTRP]/westrp-core/server/classes/sv_users.lua
@@ -1,13 +1,35 @@
 --class for users
-function User(source, identifier, fivem, group, ban, banEnd)
+function User(source, identifier, fivem, groups, ban, banEnd)
     local self
 
     self._identifier = identifier
     self._fivem = fivem
-    self._group = group
+    self._group = groups
     self._ban = ban
     self._banEnd = banEnd
     self.source = source
 
-    
+    self.Identifier = function (value)
+        if value ~= nil then
+            self._identifier = value
+        end
+    end
+
+    self.Fivem = function (value)
+        if value ~= nil then
+            self._fivem = value
+        end
+    end
+
+    self.Ban = function (value)
+        if value ~= nil & type(value) == "boolean" then
+            self._ban = value
+        end
+    end
+
+    self.Group = function (value) -- Groups: owner, developer, admin, helper, eventmaker, player
+        if value ~= nil then
+            self._group = value
+        end
+    end
 end
